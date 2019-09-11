76623d6772fc6da89a23ada1bc503b9f8c2f5d05
aljrico <alejandro.rico@supersolid.com>
2019-09-11 17:47:31 +0100

    timediaps button

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,8 +1,8 @@
 
 library(shiny)
 
-shinyServer(function(input, output, session) {
-  observeEvent(input[['mysteryboxes.button.update_data']], {
+update_balance_data_button <- function(input, output, session) {
+  observeEvent(input[["mysteryboxes.button.update_data"]], {
     shinyjs::show("loading_page")
     hs.balancedata::update_prize_tent(spreadsheet_name = input$mysteryboxes.text.spreadsheet_name, game_folder = input$mysteryboxes.combobox.game_location)
     shinyjs::hide("loading_page")
@@ -14,6 +14,23 @@ shinyServer(function(input, output, session) {
       type = "success"
     )
   })
+
+  observeEvent(input[["timediaps.button.update_data"]], {
+    shinyjs::show("loading_page")
+    hs.balancedata::update_timediaps(spreadsheet_name = input[["mysteryboxes.text.spreadsheet_name"]], game_folder = input[["mysteryboxes.combobox.game_location"]])
+    shinyjs::hide("loading_page")
+
+    sendSweetAlert(
+      session = session,
+      title = "Done!",
+      text = "Balance Data Updated",
+      type = "success"
+    )
+  })
+}
+
+shinyServer(function(input, output, session) {
+
 
 
   # Close App when closing Browser Window
