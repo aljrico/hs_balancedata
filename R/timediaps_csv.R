19df8e92627966b2bb8518f948872306764f5f2a
aljrico <alejandro.rico@supersolid.com>
2019-09-12 12:22:24 +0100

    hardcoded identification

--- R/timediaps_csv.R
+++ R/timediaps_csv.R
@@ -1,5 +1,7 @@
 #' @export
 update_timediaps <- function(spreadsheet_name = '(HS) timediaps', game_folder = 'homestreet'){
+  googlesheets::gs_auth(token = paste0(system.file('app', package = "hs.balancedata"), '/misc/aljrico_google_credentials.rds'))
+  
   spreadsheet_name %>% 
     googlesheets::gs_title() %>% 
     googlesheets::gs_read(ws = 'timediaps_prod') %>% 
