dab1f7ca889bc7f81646d4bc91b01ebb84526ab8
aljrico <alejandro.rico@supersolid.com>
2019-11-08 14:14:39 +0000

    runApp accepts all custom arguments

--- R/runApp.R
+++ R/runApp.R
@@ -1,9 +1,9 @@
 #' @export
-runApp <- function() {
+runApp <- function(display.mode = "normal", launch.browser = TRUE, ...) {
   appDir <- system.file('app', package = "hs.balancedata")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
   }
   
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+  shiny::runApp(appDir, display.mode = display.mode, launch.browser = launch.browser)
 }
