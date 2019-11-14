17b8510acbac705382cbb1c59cef3c4461323345
aljrico <alejandro.rico@supersolid.com>
2019-11-14 12:27:47 +0000

    original library not referred

--- R/update_quest_tasks.R
+++ R/update_quest_tasks.R
@@ -21,17 +21,17 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
   
   master_clean <- function(economy_file) {
     df <- economy_file %>%
-      read_excel(sheet = "MASTER", skip = 4) %>%
-      select(-`...1`, -`...2`)
+      readxl::read_excel(sheet = "MASTER", skip = 4) %>%
+      dplyr::select(-`...1`, -`...2`)
 
     events <- df %>%
-      filter(`Item Name` %>% str_detect("Event") | `Workbench Name` %>% str_detect("Bingo")) %>%
+      dplyr::filter(`Item Name` %>% stringr::str_detect("Event") | `Workbench Name` %>% stringr::str_detect("Bingo")) %>%
       .[["Workbench Name"]]
 
     events_pos <- which(df$`Workbench Name` %in% events)
     df$event <- "permanent"
     for (i in seq_along(events_pos)) {
-      df[events_pos[[i]]:(df$event %>% length()), "event"] <- events[[i]] %>% str_remove_all(" ")
+      df[events_pos[[i]]:(df$event %>% length()), "event"] <- events[[i]] %>% stringr::str_remove_all(" ")
     }
     return(df)
   }
@@ -42,21 +42,20 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
     if (this_task == "Own an item") {
       new_quest[, paste0("task", t, " skip cash") := this_design_table[[paste0("Task Skip Cost ", t)]]]
       this_specific <- this_design_table[[paste0("Task Specifics ", t)]] %>%
-        str_split("\\|") %>%
+        stringr::str_split("\\|") %>%
         unlist() %>%
-        str_trim()
+        stringr::str_trim()
 
       if (is.na(this_specific)) {
         this_specific <- this_design_table[[paste0("Task Prod ", t)]] %>%
-          str_split("\\|") %>%
+          stringr::str_split("\\|") %>%
           unlist() %>%
-          str_trim()
+          stringr::str_trim()
       }
 
       item_id <-
-        items_season
-
-      filter(Item %in% this_specific) %>%
+        items_season %>% 
+        dplyr::filter(Item %in% this_specific) %>%
         .$Id %>%
         as.numeric() %>%
         as.character() %>%
@@ -74,9 +73,9 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
         "UI/icons/Quest/quest_task_icon_",
         this_design_table[[paste0("Task Specifics ", t)]] %>%
           tolower() %>%
-          str_remove_all(" ") %>%
-          str_remove_all("\\'") %>%
-          str_trim()
+          stringr::str_remove_all(" ") %>%
+          stringr::str_remove_all("\\'") %>%
+          stringr::str_trim()
       )]
     } else if (this_task == "Collect a specific product") {
       product <- this_design_table[[paste0("Task Prod ", t)]]
@@ -86,9 +85,9 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
 
       prod_id <-
         master_file %>%
-        filter(event == (event_name %>% str_remove_all(" ") %>% str_remove_all("\\_.*"))) %>%
-        mutate(clean_name = `Item Name` %>% gsub(".*[0-9]", "", .) %>% str_trim()) %>%
-        filter(product %>% str_detect(clean_name)) %>%
+        dplyr::filter(event == (event_name %>% stringr::str_remove_all(" ") %>% stringr::str_remove_all("\\_.*"))) %>%
+        dplyr::mutate(clean_name = `Item Name` %>% gsub(".*[0-9]", "", .) %>% stringr::str_trim()) %>%
+        dplyr::filter(product %>% stringr::str_detect(clean_name)) %>%
         .[["Product"]] %>%
         .[[length(.)]] %>%
         as.character()
@@ -96,18 +95,18 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
       this_event_name <-
         this_design_table[["Event Name"]] %>%
         tolower() %>%
-        str_remove_all("\\_.*")
+        stringr::str_remove_all("\\_.*")
 
       new_quest[, paste0("task", t, " product id") := prod_id]
       new_quest[, paste0("task", t, " icon prefab") := paste0(
         "UI/icons/Quest/quest_task_icon_",
         this_design_table[[paste0("Task Specifics ", t)]] %>%
           tolower() %>%
-          str_remove_all(" ") %>%
-          str_remove_all("\\'s") %>%
-          str_remove_all("\\'") %>%
-          str_remove_all(this_event_name) %>%
-          str_trim()
+          stringr::str_remove_all(" ") %>%
+          stringr::str_remove_all("\\'s") %>%
+          stringr::str_remove_all("\\'") %>%
+          stringr::str_remove_all(this_event_name) %>%
+          stringr::str_trim()
       )]
       new_quest[, `icon prefab` := paste0("UI/icons/Quest/quest_icon_", this_design_table$`Event Name` %>% tolower())]
 
@@ -122,23 +121,23 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
     } else if (this_task == "Buy X decoration items with a limited time") {
       new_quest[, paste0("task", t, " PurchaseShopItem isTimeLimited") := TRUE]
       this_specific <- this_design_table[[paste0("Task Specifics ", t)]]
-      if (this_design_table[[paste0("Task Prod ", t)]] %>% tolower() %>% str_detect("coin")) {
+      if (this_design_table[[paste0("Task Prod ", t)]] %>% tolower() %>% stringr::str_detect("coin")) {
         new_quest[, paste0("task", t, " PurchaseShopItem useCoins") := TRUE]
       }
     } else if (this_task == "Complete X amount of event jobs") {
       new_quest[, paste0("task", t, " icon prefab") := paste0(
         "UI/icons/Quest/quest_task_icon_eventorder_",
         this_design_table[["Event Name"]] %>%
-          str_trim() %>%
-          str_remove_all("2019") %>%
-          str_remove_all("2018") %>%
-          str_remove_all("2020") %>%
-          str_remove_all("2021") %>%
-          str_remove_all("Bingo") %>%
-          str_remove_all("\\_1$") %>%
-          str_remove_all("\\_2$") %>%
-          # str_remove_all('_[^_]+$') %>%
-          str_trim() %>%
+          stringr::str_trim() %>%
+          stringr::str_remove_all("2019") %>%
+          stringr::str_remove_all("2018") %>%
+          stringr::str_remove_all("2020") %>%
+          stringr::str_remove_all("2021") %>%
+          stringr::str_remove_all("Bingo") %>%
+          stringr::str_remove_all("\\_1$") %>%
+          stringr::str_remove_all("\\_2$") %>%
+          # stringr::str_remove_all('_[^_]+$') %>%
+          stringr::str_trim() %>%
           tolower()
       )]
     } else if (this_task == "Collect a specific thought") {
@@ -148,9 +147,9 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
       if (is.na(product)) product <- this_design_table[[paste0("Task Specifics ", t)]]
 
       ing_id <- master %>%
-        filter(event == (event_name %>% str_remove_all(" ") %>% str_remove_all("\\_.*"))) %>%
-        mutate(clean_name = `Item Name` %>% gsub(".*[0-9]", "", .) %>% str_trim()) %>%
-        filter(product %>% str_detect(clean_name)) %>%
+        dplyr::filter(event == (event_name %>% stringr::str_remove_all(" ") %>% stringr::str_remove_all("\\_.*"))) %>%
+        dplyr::mutate(clean_name = `Item Name` %>% gsub(".*[0-9]", "", .) %>% stringr::str_trim()) %>%
+        dplyr::filter(product %>% stringr::str_detect(clean_name)) %>%
         .[["Thought"]] %>%
         .[[length(.)]] %>%
         as.character()
@@ -158,33 +157,33 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
       this_event_name <-
         this_design_table[["Event Name"]] %>%
         tolower() %>%
-        str_remove_all("\\_.*")
+        stringr::str_remove_all("\\_.*")
 
       new_quest[, paste0("task", t, " thought id") := ing_id]
       new_quest[, paste0("task", t, " icon prefab") := paste0(
         "UI/icons/Quest/quest_task_icon_",
         this_design_table[[paste0("Task Specifics ", t)]] %>%
           tolower() %>%
-          str_remove_all("\\'s") %>%
-          str_remove_all(" ") %>%
-          str_remove_all(this_event_name) %>%
-          str_trim()
+          stringr::str_remove_all("\\'s") %>%
+          stringr::str_remove_all(" ") %>%
+          stringr::str_remove_all(this_event_name) %>%
+          stringr::str_trim()
       )]
       new_quest[, `icon prefab` := paste0("UI/icons/Quest/quest_icon_", this_design_table$`Event Name` %>% tolower())]
     } else if (this_task == "Upgrade any Event Product using Tokens") {
       new_quest[, paste0("task", t, " icon prefab") := paste0(
         "UI/icons/Quest/quest_task_icon_eventorder_",
         this_design_table[["Event Name"]] %>%
-          str_trim() %>%
-          str_remove_all("2019") %>%
-          str_remove_all("2018") %>%
-          str_remove_all("2020") %>%
-          str_remove_all("2021") %>%
-          str_remove_all("Bingo") %>%
-          str_remove_all("\\_1$") %>%
-          str_remove_all("\\_2$") %>%
-          # str_remove_all('_[^_]+$') %>%
-          str_trim() %>%
+          stringr::str_trim() %>%
+          stringr::str_remove_all("2019") %>%
+          stringr::str_remove_all("2018") %>%
+          stringr::str_remove_all("2020") %>%
+          stringr::str_remove_all("2021") %>%
+          stringr::str_remove_all("Bingo") %>%
+          stringr::str_remove_all("\\_1$") %>%
+          stringr::str_remove_all("\\_2$") %>%
+          # stringr::str_remove_all('_[^_]+$') %>%
+          stringr::str_trim() %>%
           tolower()
       )]
     }
@@ -197,13 +196,13 @@ update_quest_tasks <- function(new_quest, seasonalquest_prod, quest_id, this_des
   for (t in maximum_number_tasks) {
     this_task_data <-
       task_types %>%
-      filter(Category == this_design_table[[paste0("Task Action ", t)]]) %>%
-      select(-Category)
+      dplyr::filter(Category == this_design_table[[paste0("Task Action ", t)]]) %>%
+      dplyr::select(-Category)
 
     colnames(this_task_data) <-
       this_task_data %>%
       colnames() %>%
-      str_remove("task ")
+      stringr::str_remove("task ")
 
     colnames(this_task_data) <-
       paste0("task", t, " ", colnames(this_task_data))
